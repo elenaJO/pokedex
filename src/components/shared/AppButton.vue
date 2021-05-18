ef143d364b1c51f405fba27efd902e1cafceda81
elenaJO <ele.lucero1993@gmail.com>
2021-05-17 19:31:28 -0500

    feat(MAQ): Maquetacion de botones

---
+++ src/components/shared/AppButton.vue
@@ -0,0 +1,49 @@
+<template>
+	<button 
+		:disabled="disabled"
+		class="app-button"
+	>
+		<slot></slot>
+		<span class="app-button__text">{{ title }}</span>
+	</button>
+</template>
+<script>
+export default {
+	name: 'AppButtons',
+	props: {
+		title: String,
+		disabled: {
+			types: Boolean,
+			default: false,
+		},
+	}
+}
+</script>
+<style lang="scss" scoped>
+
+	.app-button {
+		background-color: $black-red;
+		border: none;
+		@include border-radius(60px);
+		color: $white;
+		cursor: pointer;
+		min-width: 150px;
+		padding: 11px 20px;
+		@include transition(.5s ease-in-out);
+
+		&__text {
+			font-weight: 700;
+			line-height: $medium22;
+			margin-left: 10px;
+		}
+
+		&:disabled {
+			background-color: $light-grey;
+			cursor: not-allowed;
+		}
+
+		&:hover {
+			background-color: $medium-red;
+		}
+	}
+</style>
