876b7fef456b7b918f5ebc627076c1da990110b5
elenaJO <ele.lucero1993@gmail.com>
2021-05-17 19:49:45 -0500

    feat(MAQ): maquetacion buscador

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,19 @@
     >
       <img src="./assets/icons/all.svg">
     </AppButtons>
+    <AppSearch/>
     <!-- <router-view/> -->
   </div>
 </template>
 <script>
 import AppButtons from './components/shared/AppButton';
+import AppSearch from './components/shared/AppSearch';
 
 export default {
   name: 'App',
   components: {
     AppButtons,
+    AppSearch,
   },
 }
 </script>
