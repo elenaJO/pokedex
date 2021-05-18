aff04c15c99c72664cafd2f519ef48c4688532ce
elenaJO <ele.lucero1993@gmail.com>
2021-05-17 20:18:25 -0500

    feat(MAQ): Maquetacion listar

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,21 @@
       <img src="./assets/icons/all.svg">
     </AppButtons>
     <AppSearch/>
+    <AppList/>
     <!-- <router-view/> -->
   </div>
 </template>
 <script>
 import AppButtons from './components/shared/AppButton';
 import AppSearch from './components/shared/AppSearch';
+import AppList from './components/shared/AppList';
 
 export default {
   name: 'App',
   components: {
     AppButtons,
     AppSearch,
+    AppList,
   },
 }
 </script>
---
+++ src/components/shared/AppList.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="app-list">
+		<span class="app-list__text">Pokémon #1</span>
+		<div class="app-list__content-start">
+			<AppStart color="#BFBFBF"/>
+		</div>
+	</div>
+</template>
+<script>
+import AppStart from './AppStart';
+
+export default {
+	name: 'app-list',
+	components: {
+		AppStart,
+	},
+}
+</script>
+<style lang="scss" scoped>
+	.app-list {
+		@include flex-between();
+		background-color: $white;
+		color: $medium-grey;
+		@include border-radius(5px);
+		max-width: 570px;
+		padding: 17px 20px;
+
+		&__content-start {
+			@include flex-center();
+			background-color: $light-white;
+			@include border-radius(50%);
+			height: 44px;
+			width: 44px;
+		}
+
+		&__text {
+			font-size: $medium22;
+		}
+	}
+</style>
