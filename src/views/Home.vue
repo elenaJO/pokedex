e6df204e05fa9e441a6826245c4ad0f830ec8abe
elenaJO <ele.lucero1993@gmail.com>
2021-05-17 20:56:51 -0500

    feat(MAQ): Maquetacion pantalla home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,71 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home__content-image">
+      <img 
+        src="../assets/images/pikachu.svg" 
+        alt="circulo"
+        class="home__image"
+      >
+    </div>
+    <h1 class="home__title">Welcome to Pokédex</h1>
+    <p class="home__text">
+      The digital encyclopedia created by Professor 
+      Oak is an invaluable tool to Trainers 
+      in the Pokémon world.
+    </p>
+    <div class="home__content-btn">
+      <AppButton title="Get started"/>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AppButton from '../components/shared/AppButton'
 
 export default {
+  components: { AppButton },
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+<style lang="scss" scoped>
+.home {
+  padding: 83px 32px;
+
+  &__content-image {
+    background-image: url('../assets/images/circle.svg');
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 206px;
+    margin: 0 auto 60px auto;
+    position: relative;
+    width: 206px;
+  }
+
+  &__title {
+    color: $medium-grey;
+    line-height: 31px;
+    font-size: $large;
+    font-weight: 700;
+    margin-bottom: 30px;
+    text-align: center;
+  }
+
+  &__text {
+    color: $primary;
+    line-height: 150%;
+    margin-bottom: 50px;
+    text-align: center;
+  }
+
+  &__image {
+    bottom: -11px;
+    position: absolute;
+    right: -70px;
+  }
+
+  &__content-btn {
+    text-align: center;
+  }
+}
+</style>
