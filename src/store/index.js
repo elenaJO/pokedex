cf6ca5232d7dfd86e0cc5c0d522886e6b9be1789
elenaJO <ele.lucero1993@gmail.com>
2021-05-18 16:08:55 -0500

    feat(INT): implementacion favoritos

--- src/store/index.js
+++ src/store/index.js
@@ -6,22 +6,15 @@ Vue.use(Vuex);
 const store = new Vuex.Store(
 	{
 		state: {
-			title: 'Hola',
-			list: [
-				{
-					id: 1,
-					name: 'pikachu',
-				},
-				{
-					id: 2,
-					name: 'pikachu',
-				},
-			],
-			favotites: [],
+			list: [],
+			favorites: [],
 		},
 		getters: {
 			list(state) {
 				return state.list
+			},
+			favorites(state) {
+				return state.favorites
 			},
 		},
 		actions: {
@@ -31,10 +24,36 @@ const store = new Vuex.Store(
 				const data = await resp.json();
 				context.commit('setList', data.results);
 			},
+			getPokFavorite(context, index) {
+				context.commit('setPokFav', index);
+			},
+			getPokDeleteFavorite(context, index) {
+				context.commit('setPokDeleteFav', index);
+			},
 		},
 		mutations: {
 			setList(state, items) {
-				Vue.set(state, 'list', items);
+				const itemsNew = items.map((i) => {
+					const newItem = { ...i };
+					newItem.favorite = false;
+					return newItem;
+				});
+				state.list = itemsNew;
+			},
+			setPokFav(state, index) {
+				state.list[index].favorite = !state.list[index].favorite;
+				if (state.list[index].favorite) {
+					state.favorites.push(state.list[index]);
+				} else {
+					const indexDelete = state.favorites.findIndex(f => f.name === state.list[index].name);
+					state.favorites.splice(indexDelete, 1);
+				}
+			},
+			setPokDeleteFav(state, index) {
+				const namePok = state.favorites[index].name;
+				state.favorites.splice(index, 1);
+				const indexD = state.list.findIndex(f => f.name === namePok);
+				state.list[indexD].favorite = !state.list[indexD].favorite;
 			}
 		}
 	},
